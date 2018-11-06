0059b8a9bd2408653ba7cb65a670deacaec095b5
Finn White <37319636+finnwhite@users.noreply.github.com>
2018-11-06 13:17:34 -0800

    Add basic main menu
    Completes AddressBloc > CP01 Prompt and Menu

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-const inquirer = require( "inquirer" );
+const MenuController = require( "./controllers/MenuController.js" );
+const menu = new MenuController();
 
-const questions = [
-  {
-    type: "list",
-    name: "status",
-    message: "Are you hungry?: ",
-    choices: [ "Yes", "No" ]
-  }
-];
-
-inquirer.prompt( questions )
-  .then( ( answers ) => {
-    if ( answers.status === "Yes" ) {
-      console.log( "Then get up and eat!" );
-    } else {
-      console.log( "Then get back to work!" );
-    }
-  } )
-  .catch( ( err ) => {
-    console.log( err );
-  } );
+menu.clear();
+menu.main();
---
+++ controllers/MenuController.js
@@ -0,0 +1,57 @@
+const inquirer = require( "inquirer" );
+
+class MenuController {
+  constructor() {
+    this.mainMenuQuestions = [
+      {
+        type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from the options below: ",
+        choices: [
+          "Add new contact",
+          "Exit"
+        ]
+      }
+    ];
+    this.contacts = [];
+  }
+
+  clear() {
+    console.log( "\x1Bc" );
+  }
+
+  main() {
+    console.log( "Welcome to AddressBloc!" );
+
+    inquirer.prompt( this.mainMenuQuestions )
+      .then( ( response ) => {
+        switch ( response.mainMenuChoice ) {
+          case "Add new contact":
+            this.addContact();
+            break;
+          case "Exit":
+            this.exit();
+            break;
+          default:
+            console.log( "Invalid input" );
+            this.main();
+        }
+      } )
+      .catch( ( err ) => {
+        console.log( err );
+      } );
+  }
+
+  addContact() {
+    this.clear();
+    console.log( "Contact added" );
+    this.main();
+  }
+
+  exit() {
+    console.log( "Thank you for using AddressBloc!" );
+    process.exit();
+  }
+}
+
+module.exports = MenuController;
